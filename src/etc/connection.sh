d8e31e35b013f71784e7628826cf757b330d2ff5
SUVRADEEP BASAK <suvradeepbasak2024@outlook.in>
2026-03-14 21:39:23 +0000

    p

--- src/etc/connection.sh
+++ src/etc/connection.sh
@@ -2,7 +2,7 @@
 
 # Check github connection stable or not:
 check_connection() {
-	wget -q $(curl -s "https://api.github.com/repos/revanced/revanced-patches/releases/tags/v4.17.0" | jq -r '.assets[] | select(.name == "patches.json") | .browser_download_url') || rm -f "patches.json"
+	wget -q $(curl -s "https://api.github.com/repos/revanced/revanced-patches/releases/latest" | jq -r '.assets[] | select(.name == "patches.json") | .browser_download_url') || rm -f "patches.json"
 	if [ -f patches.json ]; then
 		echo "internet_error=0" >> $GITHUB_OUTPUT
 		echo -e "\e[32mGithub connection OK\e[0m"
